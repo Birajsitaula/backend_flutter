f9ab5f419df1c69391797d54326da1fd33bf17a9
Birajsitaula <birajsitaula7@gmail.com>
2025-02-13 11:45:08 +0545

    crating the database

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import express from "express";
 import "dotenv/config";
+import connection from "../models/index.js";
+import userRouter from "../router/userRouter.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/user", userRouter);
+const port = process.env.PORT;
 
-app.listen(8000, () => {
-  console.log("Server running successfully");
+app.listen(port, async () => {
+  try {
+    await connection.authenticate();
+    await connection.sync();
+    console.log("Server is created successfully");
+  } catch (err) {
+    console.log("There is an error while creating the server ", err);
+  }
 });
